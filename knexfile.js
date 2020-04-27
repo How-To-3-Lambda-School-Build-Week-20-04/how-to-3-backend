ce952241d5ecf6cd1043b3d978b71fa48ea21792
TREY <hugh.a.lloyd@gmail.com>
2020-04-27 14:53:30 -0500

    installed pg for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,48 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   // npx knex migrate:latest
   // npx knex seed:run
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
       filename: "./data/project.db3",
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds'
-    }
+      directory: "./data/seeds",
+    },
   },
 
   // set up the testing database
   // npx knex migrate:latest --env testing
   // npx knex seed:run --env testing
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
       filename: "./data/test_project.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds'
-    }
+      directory: "./data/seeds",
+    },
   },
 
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: './data/migrations'
-    }
-  }
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };
