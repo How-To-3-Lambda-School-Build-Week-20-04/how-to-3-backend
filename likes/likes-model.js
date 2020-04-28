a74c3087c5c30793fef9cebc117b8c13752f5882
TREY <hugh.a.lloyd@gmail.com>
2020-04-28 18:53:10 -0500

    edited table and got add likes working

--- likes/likes-model.js
+++ likes/likes-model.js
@@ -1,13 +1,21 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
+const { findByID } = require("../howto/howto-model");
 
 // return user's like on a post
 
 // return all likes on a post
 
 // add a like to a post
+function addLike(like) {
+  console.log(like, "like from model");
+  return db("likes")
+    .insert(like, "id")
+    .then(([id]) => {
+      console.log(id, "id from model");
+      return db("likes").where({ id });
+    });
+}
 
 // remove a like from a post
 
-module.exports = {
-
-};
+module.exports = { addLike };
--- likes/likes-router.js
+++ likes/likes-router.js
@@ -1,11 +1,23 @@
-const router = require('express').Router();
-const Likes = require('./likes-model.js');
+const router = require("express").Router();
+const Likes = require("./likes-model.js");
 
-// all routes are protected by requiring a token currently
-// certain routes might require further middleware before making actions to db
+router.get("/", async (req, res) => {
+  res.json({ message: "likes router here" });
+});
 
-router.get('/', async (req, res) => {
-  res.json({ message: "likes router here" })
-})
+router.post("/", (req, res) => {
+  const likeData = req.body;
+  console.log(likeData, "likeData req from post");
+  Likes.addLike(likeData)
+    .then((data) => {
+      console.log(data, "data from add promise");
+      !data
+        ? res.status(400).json({ message: "improper request" })
+        : res.status(201).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
 
 module.exports = router;
