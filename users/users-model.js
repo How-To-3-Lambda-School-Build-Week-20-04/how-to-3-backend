22252fc89e0ea937a5d61d0591ffd207e4e582fd
Jesse <fkng3rmz@gmail.com>
2020-04-28 12:22:27 -0400

    adds user endpoints

--- users/users-model.js
+++ users/users-model.js
@@ -2,15 +2,29 @@ const db = require('../data/dbConfig.js');
 
 // this file is used by auth-router
 
+// returns all users
 function getAll() {
   return db('user')
+  .select('id','username','email')
 }
 
-function getBy(filter) {
+// this is used in auth-router
+// returns a single user by their id
+function getBy(id) {
   return db('user')
-    .where(filter)
+    .where({id})
+    .select('id','username','email')
 }
 
+// returns a user's posts by their id
+function getUserPosts(user_id) {
+  return db('howto as h')
+    .join('user as u', 'h.user_id', 'u.id')
+    .where({user_id})
+    .select('u.id as user_id', 'h.id as howto_id', 'u.username', 'h.title', 'h.post', 'h.created_at')
+}
+
+// this is also used in auth-router
 async function add(user) {
   const [id] = await db('user')
     .insert(user)
@@ -39,6 +53,7 @@ function remove(id) {
 module.exports = {
   getAll,
   getBy,
+  getUserPosts,
   add,
   update,
   remove
