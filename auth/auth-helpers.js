62274ff726aa0ae58ee9b0481c8dd2f4c06309b2
Jesse <fkng3rmz@gmail.com>
2020-04-30 12:03:24 -0400

    moves gentoken to auth-helper

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -16,8 +16,7 @@ function restricted (req, res, next) {
 }
 
 function bodyCheck (req, res, next) {
-  const email = req.body.email
-  const password = req.body.password
+  const { email, password } = req.body
 
   if (!req.body || !email || !req.body.username || !password) {
     res.status(400).json({ error: "Missing required information." })
@@ -32,7 +31,28 @@ function bodyCheck (req, res, next) {
   }
 }
 
+function genToken(user) {
+  if (
+    !user ||
+    typeof user.id === "undefined" ||
+    typeof user.username === "undefined"
+  ) {
+    new error("username and id needed");
+  }
+  const payload = {
+    userId: user.id,
+    username: user.username,
+  };
+  const secret = process.env.TOKEN_SECRET;
+  const options = {
+    expiresIn: process.env.TOKEN_LIFE || "12h",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 module.exports = {
   restricted,
-  bodyCheck
+  bodyCheck,
+  genToken
 }
