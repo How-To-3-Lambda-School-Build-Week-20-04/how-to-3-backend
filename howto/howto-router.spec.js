216c92832491d7f2a3f674e9ecb74a43f5ffe395
Jesse <fkng3rmz@gmail.com>
2020-04-30 13:08:19 -0400

    refactors tests to truncate test db after each test

--- howto/howto-router.spec.js
+++ howto/howto-router.spec.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 const request = require('supertest')
 const server = require('../api/server.js')
@@ -7,11 +8,7 @@ const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJuYW1l
 
 
 describe('howto router', () => {
-  afterAll(async () => {
-    /* await db('likes').truncate()
-    await db('howto_category').truncate()
-    await db('category').truncate()
-    */
+  beforeEach(async () => {
     await db('howto').truncate()
   })
 
@@ -24,6 +21,25 @@ describe('howto router', () => {
           expect(res.body).toHaveLength(0)
         })
     });
-  });
+  }); // end of get
+
+  describe('POST /', () => {
+    it('should return some thicc how-to information after submit', async () => {
+      return request(server)
+        .post('/api/howto')
+        .set({ authorization: token })
+        .send({
+          "title":"edgar's new how-to",
+          "post":"this probably needs more steps",
+          "user_id":1
+        })
+        .then(res => {
+          console.log(res.body)
+          expect(res.body.id).toBe(1)
+          expect(res.body.likes).toBe(0)
+          expect(res.body.categories).toHaveLength(0)
+        })
+    });
+  }); // end of post
 
 })
